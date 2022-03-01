8cd81042d13f9b34c6f4e316a83e80fe02d30c28
naili95 <nailinu95@gmail.com>
2022-03-01 10:27:32 +0700

    first commit

---
+++ src/com/juaracoding/main/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.juaracoding.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Nama Saya: Naili");
+//		System.out.println("Alamat : Bitung");
+//		System.out.println("Telp : 010101");
+		boolean STNK = true;
+
+		boolean SIM = false;
+
+		if(SIM == true){
+
+		if( STNK == true ) {
+
+		System.out.println("Tidak ditilang!");
+
+		}
+
+		} else {
+
+		System.out.println("Anda ditilang!");
+
+		}
+	}
+
+}
